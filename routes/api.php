159e2e72283b43dca1aeaf798e22d4164c7a8a7a
WilsonKinyua <wilsonkinyuam@gmail.com>
2022-03-21 09:41:35 +0300

    error commented out

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MpesePaymentController;
 
-Route::any('/ipns/{daraja}/blocks-rent-payment/{action}', MpesePaymentController::class);
+// Route::any('/ipns/{daraja}/blocks-rent-payment/{action}', MpesePaymentController::class);
 
 Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin', 'middleware' => ['auth:sanctum']], function () {
 });
